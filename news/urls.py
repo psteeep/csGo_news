f0d396d796eff7c6c1a09f2131625c9388293d6f
psteeep <psteeep@users.noreply.github.com>
2022-04-01 20:00:27 +0300

    ready site teamplate

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('news_home', views.news_home, name='news_home'),
+    path('', views.news_home, name='news_home'),
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailNew.as_view(), name='news-detail'),
+    path('<int:pk>/update', views.NewsUpdateNew.as_view(), name='news-update'),
+    path('<int:pk>/delete', views.NewsDeleteNew.as_view(), name='news-delete')
 ]
+
